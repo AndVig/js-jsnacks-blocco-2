01d888089aa9accf27fe151f04fd347c7eccba69
AndVig <andrea.vigna2@gmail.com>
2024-05-07 10:29:01 +0200

    add some rand

--- snack-4/JS/script.js
+++ snack-4/JS/script.js
@@ -1,6 +1,11 @@
-var a=new Array(5);
+
+var rand=Math.floor(Math.random()*10)+1;
+var a=new Array(rand);
 var b=new Array();
 let i=0;
 while (b.length<a.length){
+    i=Math.floor(Math.random()*10)+1;
     b.push(i);
 }
+console.log(a, a.length);
+console.log(b, b.length);
