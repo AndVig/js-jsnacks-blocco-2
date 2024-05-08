26b869ed0575c775c5319d0ef040773df8f0cad8
AndVig <andrea.vigna2@gmail.com>
2024-05-08 16:36:50 +0200

    complete snack-a

--- snack-a/JS/script.js
+++ snack-a/JS/script.js
@@ -1,1 +1,15 @@
 
+function contaElementi(array){
+    let count=array.length;
+    if(array=''){
+        return console.log('array vuoto');
+    }else{
+        for(let i=0;i<array.length;i++){
+            if (array[i]=''){
+                count--
+                console.log(count);
+            }
+        }
+        return count;
+    }
+}
